38bcca863c1169fcff29626a3f73fd564be14d3a
Callasaurus <c.sullivandrage@outlook.com>
2022-12-30 16:48:59 +0000

    finishing up about

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -4,6 +4,7 @@ import soc from '../images/soc.png'
 import yougov from '../images/yougov.png'
 import cello from '../images/cello.png'
 import uob from '../images/uob.png'
+import Footer from './Footer'
 
 function About() {
   return (
@@ -14,7 +15,15 @@ function About() {
       <div className='about-items'>
         <h1 className='about-h1'> The School of Code </h1>
         <hr className='hr-item-about' />
-        <p className='about-p'>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum lectus a massa interdum volutpat. Suspendisse sagittis, dui scelerisque porttitor tempus, enim odio bibendum diam, sed blandit purus dui sed lectus. Nullam eleifend auctor neque, quis tincidunt lorem pretium eu. Quisque porta bibendum risus non imperdiet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris elementum felis non vestibulum lacinia. Donec ante ipsum, rhoncus at elit vitae, molestie pulvinar diam. In erat ligula, tristique a tortor quis, vehicula condimentum leo. Vivamus cursus efficitur arcu, nec pretium libero viverra sed. Integer eu posuere tellus. Nunc non sem et arcu fermentum efficitur. Quisque tempor erat vulputate, iaculis libero non, rhoncus justo. Etiam ut faucibus lectus. Vestibulum non orci et dui mollis mollis quis quis tellus.Nunc quis magna quam. Praesent pharetra gravida tortor, ac efficitur quam semper a. Pellentesque volutpat ex at mauris cursus varius. Phasellus nec sem elit. Nunc ut congue diam. Mauris viverra diam eu metus interdum malesuada. Sed laoreet purus non enim ultrices ultricies.</p>
+        <p className='about-p'>  Previous to my coding journey, I spent a few years working in market research, which was partly influenced by the fact that I did a research-focused masters.
+          Although I enjoyed what I did, I always felt like I wasn't challenging myself enough and always planned on making a move into tech when I had a chance.
+          <br /><br />
+          I've always been super interested in tech (mainly through gaming) and I'm a featured co-author on <a href='https://policy.bristoluniversitypress.co.uk/virtual-reality-methods' target="_blank" rel="noreferrer"> published book on Virtual Reality </a>
+          and how it can be used in social science research. I found the <a href='https://www.schoolofcode.co.uk/' target="_blank" rel="noreferrer"> School of Code </a> through a mutual friend who was a graduated bootcamper, 
+          and felt it was the perfect opportunity for me to make my first steps into the tech world!
+          <br /><br />
+          The School of code is a coding bootcamp that is specialised for full-stack JavaScript development. The Curriculum includes: HTML, CSS, JavaScript, TypeScript, React.js, Node.js, APIs and Databases, PostgreSQL, Cloud Hosting, GitHub, Figma, Software deployment, 
+          Testing, Debugging and much more. There is an emphasis on teamwork (frequent group work and pair programming with a new partner each week), problem solving, computational thinking and industry best practices(such as AGILE working). </p>
         <hr className='hr-item-about' />
         <img className='about-images' src={soc} alt=''></img>
       </div>
@@ -24,7 +33,15 @@ function About() {
       <div className='about-items'>
         <h1 className='about-h1'> YouGov </h1>
         <hr className='hr-item-about' />
-        <p className='about-p'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum lectus a massa interdum volutpat. Suspendisse sagittis, dui scelerisque porttitor tempus, enim odio bibendum diam, sed blandit purus dui sed lectus. Nullam eleifend auctor neque, quis tincidunt lorem pretium eu. Quisque porta bibendum risus non imperdiet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris elementum felis non vestibulum lacinia. Donec ante ipsum, rhoncus at elit vitae, molestie pulvinar diam. In erat ligula, tristique a tortor quis, vehicula condimentum leo. Vivamus cursus efficitur arcu, nec pretium libero viverra sed. Integer eu posuere tellus. Nunc non sem et arcu fermentum efficitur. Quisque tempor erat vulputate, iaculis libero non, rhoncus justo. Etiam ut faucibus lectus. Vestibulum non orci et dui mollis mollis quis quis tellus.Nunc quis magna quam. Praesent pharetra gravida tortor, ac efficitur quam semper a. Pellentesque volutpat ex at mauris cursus varius. Phasellus nec sem elit. Nunc ut congue diam. Mauris viverra diam eu metus interdum malesuada. Sed laoreet purus non enim ultrices ultricies. </p>
+        <p className='about-p'> Having spent two years working at Cello Health Insight, I felt it was time for me to make a change and move to another market research company that were researching things that were of more interest and
+          importance to me. I made the move to YouGov - a company that research a wide variety of different topics for a range of industries, including: social research; charity research; public sector research and private sector research.
+          <br /><br />
+          My responsibilities included: conducting fieldwork (interviews, focus groups, online communities and more); analysing in-house research and producing presentations and insights for clients; presenting data to clients; creating research
+          materials (e.g. recruitment screeners, discussion guides) and handling recruitment via YouGov's panel and by using their bespoke scripting software and language.
+          <br /><br />
+          Over the time I spent working with the scripting software I gained
+          a passion for it (learning the syntax, problem-solving, improving and finding ways to streamline the recruitment process). This sparked my interest in a career as a software developer.
+        </p>
         <hr className='hr-item-about' />
         <img className='about-images' src={yougov} alt=''></img>
       </div>
@@ -32,9 +49,13 @@ function About() {
       <hr className='hr-about' />
 
       <div className='about-items'>
-        <h1 className='about-h1'> Cello Health Insight </h1>
+        <h1 className='about-h1'> Cello Health Insight</h1>
         <hr className='hr-item-about' />
-        <p className='about-p'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum lectus a massa interdum volutpat. Suspendisse sagittis, dui scelerisque porttitor tempus, enim odio bibendum diam, sed blandit purus dui sed lectus. Nullam eleifend auctor neque, quis tincidunt lorem pretium eu. Quisque porta bibendum risus non imperdiet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris elementum felis non vestibulum lacinia. Donec ante ipsum, rhoncus at elit vitae, molestie pulvinar diam. In erat ligula, tristique a tortor quis, vehicula condimentum leo. Vivamus cursus efficitur arcu, nec pretium libero viverra sed. Integer eu posuere tellus. Nunc non sem et arcu fermentum efficitur. Quisque tempor erat vulputate, iaculis libero non, rhoncus justo. Etiam ut faucibus lectus. Vestibulum non orci et dui mollis mollis quis quis tellus.Nunc quis magna quam. Praesent pharetra gravida tortor, ac efficitur quam semper a. Pellentesque volutpat ex at mauris cursus varius. Phasellus nec sem elit. Nunc ut congue diam. Mauris viverra diam eu metus interdum malesuada. Sed laoreet purus non enim ultrices ultricies. </p>
+        <p className='about-p'> After finishing my research-focused masters and gaining a keen interest in conducting research, I ventured into the field of market research with Cello Health Insight. At the time of joining,
+          Cello Health Insight were ranked as one of the Sunday Times top 100 companies to work for and were producing interesting and important research for the healthcare and pharmaceutical industry.
+          <br /><br />
+          During my time at Cello, I primarily worked in Qualitative market research, conducting: fieldwork with a variety of different people (e.g. doctors, patients, stakeholders and more); analysing datasets from multiple markets
+          and producing actionable insights for clients. </p>
         <hr className='hr-item-about' />
         <img className='about-images' src={cello} alt=''></img>
       </div>
@@ -42,12 +63,21 @@ function About() {
       <hr className='hr-about' />
 
       <div className='about-items'>
-        <h1 className='about-h1'> The University of Birmingham </h1>
+        <h1 className='about-h1'> The University of Birmingham</h1>
         <hr className='hr-item-about' />
-        <p className='about-p'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum lectus a massa interdum volutpat. Suspendisse sagittis, dui scelerisque porttitor tempus, enim odio bibendum diam, sed blandit purus dui sed lectus. Nullam eleifend auctor neque, quis tincidunt lorem pretium eu. Quisque porta bibendum risus non imperdiet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris elementum felis non vestibulum lacinia. Donec ante ipsum, rhoncus at elit vitae, molestie pulvinar diam. In erat ligula, tristique a tortor quis, vehicula condimentum leo. Vivamus cursus efficitur arcu, nec pretium libero viverra sed. Integer eu posuere tellus. Nunc non sem et arcu fermentum efficitur. Quisque tempor erat vulputate, iaculis libero non, rhoncus justo. Etiam ut faucibus lectus. Vestibulum non orci et dui mollis mollis quis quis tellus.Nunc quis magna quam. Praesent pharetra gravida tortor, ac efficitur quam semper a. Pellentesque volutpat ex at mauris cursus varius. Phasellus nec sem elit. Nunc ut congue diam. Mauris viverra diam eu metus interdum malesuada. Sed laoreet purus non enim ultrices ultricies.</p>
+        <p className='about-p'> During my time at the University of Birmigham I undertook a BSc in Geography and Economics (2:1) and an MSc in Research in Human Geography (Distinction).
+          <br /><br />
+          BSc Geography and Economics offered me the analytical rigor of Economics and provided me with a firm understanding of advanced economic and statistical models.
+          It also exposed me to cutting-edge debates in Human Geography, Environmental Science and related fields.
+          <br /><br />
+          MSc Research in Human Geography combines advanced interdisciplinary expertise from Geography, Political Science and International Studies. The course also provided me with advanced training in qualitative and
+          quantitative social science research methodologies and their application in social research.
+        </p>
         <hr className='hr-item-about' />
         <img className='about-images' src={uob} alt=''></img>
       </div>
+
+      <Footer />
 
     </div>
   )
--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Header.css'
 import Calla from '../images/Calla.jpg'
+import html from '../images/html.png'
+import css from '../images/css.png'
+import js from '../images/js.png'
+import ts from '../images/ts.png'
+import react from '../images/react.png'
+import node from '../images/node.png'
 
 function Header() {
   return (
@@ -8,6 +14,17 @@ function Header() {
 
       <div className='text'>
         <h1 id='h1-tag'> Hi, I'm Calla, a junior software developer on the 16-week School of Code bootcamp</h1>
+
+
+        <div className='stack-icons'>
+          <img src={html} alt=''></img>
+          <img src={css} alt=''></img>
+          <img src={js} alt=''></img>
+          <img src={ts} alt=''></img>
+          <img src={react} alt=''></img>
+          <img src={node} alt=''></img>
+        </div>
+
         <p id='p-tag'> I'm an aspiring software developer who is currently on the <a href='https://www.schoolofcode.co.uk/' target="_blank" rel="noreferrer"> School of Code </a> 16-week bootcamp.
           Previous to my coding journey, I spent a few years working in market research, which was partly influenced by the fact that I did a research-focused masters.
           Although I enjoyed what I did, I always felt like I wasn't challenging myself enough and always planned on making a move into tech when I had a chance.
@@ -16,6 +33,7 @@ function Header() {
           <br /><br />
           This page gives you a glimpse into my journey from never seeing a line of code prior to the course, to where I am now. Feel free to have a look at some of my projects, my CV and don't hesitate to get in touch!
         </p>
+
 
         <div>
           <a id='a-tag' href='https://pdfhost.io/v/2dEXrThRO_CV_Calla_SullivanDrage_Dec22' target="_blank" rel="noreferrer">
