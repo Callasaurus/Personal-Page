6e1caf8f6552ce85ab6816d5b7fb5ddb2d1e5f92
Callasaurus <c.sullivandrage@outlook.com>
2022-12-31 16:43:10 +0000

    contact page

--- my-app/src/Components/Contact.js
+++ my-app/src/Components/Contact.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import './Contact.css'
+import deskman from '../images/deskman.png'
+import Footer from './Footer'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-main'>
+
+      <h1 className='contact-title'> CONTACT </h1>
+
+      <div className='contact-info'>
+        <img src={deskman} alt=''></img>
+
+        <div className='contact-buttons'>
+          <a href='https://www.linkedin.com/in/calla-sullivan-drage-88a7a5139/' target="_blank" rel="noreferrer">
+            <button className='indiv-buttons'> Reach me on LinkedIn</button>
+          </a>
+          <a href='mailto: c.sullivandrage@outlook.com' target="_blank" rel="noreferrer">
+            <button className='indiv-buttons'> Email me </button>
+          </a>
+        </div>
+      </div>
+
+      <Footer/>
+
+    </div>
   )
 }
 
